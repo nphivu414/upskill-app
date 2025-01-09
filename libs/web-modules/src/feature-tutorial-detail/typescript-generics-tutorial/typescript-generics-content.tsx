143f7e658ced31c7e7b2d99842b2b2f11a726104
Vu Nguyen <nphivu414@gmail.com>
2025-01-09 07:06:40 +0700

    - Update content

--- libs/web-modules/src/feature-tutorial-detail/typescript-generics-tutorial/typescript-generics-content.tsx
+++ libs/web-modules/src/feature-tutorial-detail/typescript-generics-tutorial/typescript-generics-content.tsx
@@ -1,9 +1,21 @@
+'use client';
+
 import React from 'react';
+import Editor from '@monaco-editor/react';
 
 import { CodeWithAccoridions } from '../code-renderer';
 import { Code } from '../content-ts-code-block';
 import TypescriptGenericsMdx from './content/main-content.mdx';
 
 export const TypescriptGenericsContent = () => {
-  return <TypescriptGenericsMdx components={{ CodeWithAccoridions, Code }} />;
+  return (
+    <>
+      <TypescriptGenericsMdx components={{ CodeWithAccoridions, Code }} />
+      <Editor
+        height="90vh"
+        defaultLanguage="typescript"
+        defaultValue="const a: string = 'test'"
+      />
+    </>
+  );
 };
