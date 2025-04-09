5ed1ac88fb77b94c03132cc7264b33fd906eca25
Vu Nguyen <nphivu414@gmail.com>
2025-04-09 22:03:23 +0700

    Remove unused OpenGraph image components and add pinning styles to DataTable for improved column management

--- libs/web-modules/src/feature-showcase/data-table/data-table.tsx
+++ libs/web-modules/src/feature-showcase/data-table/data-table.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import {
+  Column,
   ColumnDef,
   ColumnPinningState,
   flexRender,
@@ -74,8 +75,31 @@ export const columns: ColumnDef<Employee>[] = [
 export const DataTable = () => {
   const [columnPinning, setColumnPinning] = React.useState<ColumnPinningState>({
     left: ['name'],
-    right: [],
+    right: [''],
   });
+
+  const getCommonPinningStyles = (column: Column<Employee>): CSSProperties => {
+    const isPinned = column.getIsPinned();
+    const isLastLeftPinnedColumn =
+      isPinned === 'left' && column.getIsLastColumn('left');
+    const isFirstRightPinnedColumn =
+      isPinned === 'right' && column.getIsFirstColumn('right');
+
+    return {
+      boxShadow: isLastLeftPinnedColumn
+        ? '-4px 0 4px -4px gray inset'
+        : isFirstRightPinnedColumn
+        ? '4px 0 4px -4px gray inset'
+        : undefined,
+      left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
+      right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
+      opacity: isPinned ? 0.95 : 1,
+      position: isPinned ? 'sticky' : 'relative',
+      width: column.getSize(),
+      zIndex: isPinned ? 1 : 0,
+      backgroundColor: isPinned ? 'Background' : 'transparent',
+    };
+  };
 
   const table = useReactTable({
     data: mockEmployees,
@@ -89,13 +113,17 @@ export const DataTable = () => {
 
   return (
     <div>
-      <Table>
+      <Table className="!border-separate border-spacing-0 p-4">
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
+                const { column } = header;
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead
+                    key={header.id}
+                    style={{ ...getCommonPinningStyles(column) }}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -115,11 +143,20 @@ export const DataTable = () => {
                 key={row.id}
                 data-state={row.getIsSelected() && 'selected'}
               >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+                {row.getVisibleCells().map((cell) => {
+                  const { column } = cell;
+                  return (
+                    <TableCell
+                      key={cell.id}
+                      style={{ ...getCommonPinningStyles(column) }}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  );
+                })}
               </TableRow>
             ))
           ) : (
