a93ed0361e0177f7c77edb30cf02ae92ff19f006
Vu Nguyen <nphivu414@gmail.com>
2025-04-13 15:51:17 +0700

    refactor(data-table): simplify DataTablePagePreview by removing unused props and components

--- libs/tutorial-elements/src/lib/pages/data-table-page/demo/data-table-page-preview.tsx
+++ libs/tutorial-elements/src/lib/pages/data-table-page/demo/data-table-page-preview.tsx
@@ -1,40 +1,12 @@
 'use client';
 
-import {
-  getGithubSourceUrl,
-  getStorybookUrl,
-  TUTORIAL_REPO,
-  TUTORIAL_STORIES,
-} from '@upskill-app/shared';
-import { LivePreview } from '@upskill-app/ui/web';
-import { useTheme } from 'next-themes';
-
 import { MockupBrowser } from '../../../components';
 import { DataTablePage } from '../data-table-page';
 
-type DataTablePagePreviewProps = {
-  showStorySourceLink?: boolean;
-};
-
-export const DataTablePagePreview = ({
-  showStorySourceLink,
-}: DataTablePagePreviewProps) => {
-  const { theme } = useTheme();
+export const DataTablePagePreview = () => {
   return (
-    <LivePreview
-      storybookUrl={getStorybookUrl(
-        TUTORIAL_STORIES.FLIGHT_BOOKING_UI.FLIGHT_BOOKING_PAGE,
-        theme
-      )}
-      sourceUrl={getGithubSourceUrl(
-        TUTORIAL_REPO.FLIGHT_BOOKING_UI,
-        'blob/main/src/components/flight-booking/flight-booking-page.tsx'
-      )}
-      showStorySourceLink={showStorySourceLink}
-    >
-      <MockupBrowser className="w-full max-w-full">
-        <DataTablePage />
-      </MockupBrowser>
-    </LivePreview>
+    <MockupBrowser className="w-full max-w-full">
+      <DataTablePage />
+    </MockupBrowser>
   );
 };
